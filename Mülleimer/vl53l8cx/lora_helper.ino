b49a39b26eb491c48dc07aeb19f810243336fede
PaulaScharf <paulascharf@gmx.de>
2023-11-03 17:49:40 +0100

    update trashcan example

---
+++ Mülleimer/vl53l8cx/lora_helper.ino
@@ -0,0 +1,88 @@
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+char* slave_name = "slv1";
+
+#define TX_INTERVAL 2000
+
+#define VCC_ENABLE 41
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+  .nss = 34,
+  .rxtx = LMIC_UNUSED_PIN,
+  .rst = LMIC_UNUSED_PIN,
+  .dio = {33, 33, LMIC_UNUSED_PIN},
+};
+
+
+// These callbacks are only used in over-the-air activation, so they are
+// left empty here (we cannot leave them out completely unless
+// DISABLE_JOIN is set in arduino-lmoc/project_config/lmic_project_config.h,
+// otherwise the linker will complain).
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
+void onEvent (ev_t ev) {
+}
+
+osjob_t txjob;
+osjob_t timeoutjob;
+
+// Transmit the given string and call the given function afterwards
+void tx(const char *str, osjobcb_t func) {
+  os_radio(RADIO_RST); // Stop RX first
+  delay(1); // Wait a bit, without this os_radio below asserts, apparently because the state hasn't changed yet
+  LMIC.dataLen = 0;
+  while (*str)
+    LMIC.frame[LMIC.dataLen++] = *str++;
+  LMIC.osjob.func = func;
+  os_radio(RADIO_TX);
+}
+
+// Enable rx mode and call func when a packet is received
+void rx(osjobcb_t func) {
+  LMIC.osjob.func = func;
+  LMIC.rxtime = os_getTime(); // RX _now_
+  // Enable "continuous" RX (e.g. without a timeout, still stops after
+  // receiving a packet)
+  os_radio(RADIO_RXON);
+}
+
+void setup_lora() {
+  // LMIC initialize runtime env
+  pinMode(VCC_ENABLE, OUTPUT);
+  digitalWrite(VCC_ENABLE, LOW);
+  delay(1000);
+
+  // initialize runtime env
+  os_init();
+  // Use a frequency in the g3 which allows 10% duty cycling.
+  LMIC.freq = 869525000;
+  // Use a medium spread factor. This can be increased up to SF12 for
+  // better range, but then, the interval should be (significantly)
+  // raised to comply with duty cycle limits as well.
+  LMIC.datarate = DR_SF9;
+  // Maximum TX power
+  LMIC.txpow = 27;
+
+
+  // disable RX IQ inversion
+  LMIC.noRXIQinversion = true;
+
+  // This sets CR 4/5, BW125 (except for EU/AS923 DR_SF7B, which uses BW250)
+  LMIC.rps = updr2rps(LMIC.datarate);
+
+  Serial.print("Frequency: "); Serial.print(LMIC.freq / 1000000);
+            Serial.print("."); Serial.print((LMIC.freq / 100000) % 10);
+            Serial.print("MHz");
+  Serial.print("  LMIC.datarate: "); Serial.print(LMIC.datarate);
+  Serial.print("  LMIC.txpow: "); Serial.println(LMIC.txpow);
+
+  // This sets CR 4/5, BW125 (except for DR_SF7B, which uses BW250)
+  LMIC.rps = updr2rps(LMIC.datarate);
+
+  // disable RX IQ inversion
+  LMIC.noRXIQinversion = true;
+}
---
+++ Mülleimer/vl53l8cx/vl53l8cx.ino
@@ -0,0 +1,286 @@
+/**
+ ******************************************************************************
+ * @file    VL53L8CX_ThresholdDetection.ino
+ * @author  STMicroelectronics
+ * @version V1.0.0
+ * @date    12 June 2023
+ * @brief   Arduino test application for the STMicrolectronics VL53L8CX
+ *          proximity sensor satellite based on FlightSense.
+ *          This application makes use of C++ classes obtained from the C
+ *          components' drivers.
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; COPYRIGHT(c) 2021 STMicroelectronics</center></h2>
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *   3. Neither the name of STMicroelectronics nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
+ */
+/*
+ * To use these examples you need to connect the VL53L8CX satellite sensor directly to the Nucleo board with wires as explained below:
+ * pin 1 (SPI_I2C_n) of the VL53L8CX satellite connected to pin GND of the Nucleo board
+ * pin 2 (LPn) of the VL53L8CX satellite connected to pin A3 of the Nucleo board
+ * pin 3 (NCS) not connected
+ * pin 4 (MISO) not connected
+ * pin 5 (MOSI_SDA) of the VL53L8CX satellite connected to pin D14 (SDA) of the Nucleo board
+ * pin 6 (MCLK_SCL) of the VL53L8CX satellite connected to pin D15 (SCL) of the Nucleo board
+ * pin 7 (PWREN) of the VL53L8CX satellite connected to pin D11 of the Nucleo board
+ * pin 8 (I0VDD) of the VL53L8CX satellite not connected
+ * pin 9 (3V3) of the VL53L8CX satellite connected to 3V3 of the Nucleo board
+ * pin 10 (1V8) of the VL53L8CX satellite not connected
+ * pin 11 (5V) of the VL53L8CX satellite not connected 
+ * GPIO1 of VL53L8CX satellite connected to A2 pin of the Nucleo board (not used)
+ * GND of the VL53L8CX satellite connected to GND of the Nucleo board
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include <Arduino.h>
+#include <Wire.h>
+#include <vl53l8cx_class.h>
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+
+#define DEV_I2C Wire
+#define SerialPort Serial
+
+#define LPN_PIN 4
+#define I2C_RST_PIN -1
+#define PWREN_PIN 2
+#define INT_PIN 3
+
+void measure(void);
+void print_result(VL53L8CX_ResultsData *Result);
+String dataStr = "";
+
+// Component.
+VL53L8CX sensor_VL53L8CX_top(&DEV_I2C, LPN_PIN, I2C_RST_PIN);
+VL53L8CX_DetectionThresholds thresholds[VL53L8CX_NB_THRESHOLDS];
+const int res = VL53L8CX_RESOLUTION_8X8;
+long trashcan_dimensions[res];
+long testi[res];
+
+bool EnableAmbient = false;
+bool EnableSignal = false;
+char report[256];
+volatile int interruptCount = 0;
+
+bool trashcan_full = false;
+
+bool lora_in_progress = false;
+
+uint8_t NewDataReady;
+uint8_t status;
+uint8_t number_of_zones = res;
+int8_t i, j, k, l;
+uint8_t zones_per_line = (number_of_zones == 16) ? 4 : 8;
+
+/* Setup ---------------------------------------------------------------------*/
+
+void setup()
+{
+
+  // Initialize serial for output.
+  SerialPort.begin(9600);
+  while(!SerialPort);
+
+  // Enable PWREN pin if present
+  if (PWREN_PIN >= 0) {
+    pinMode(PWREN_PIN, OUTPUT);
+    digitalWrite(PWREN_PIN, HIGH);
+    delay(10);
+  }
+
+  // init Lora
+  Serial.println("initing Lora");
+  setup_lora();
+
+  // Initialize I2C bus.
+  DEV_I2C.begin(39,40);
+  DEV_I2C.setClock(1000000); //Sensor has max I2C freq of 1MHz
+
+  
+  // Configure VL53L8CX component.
+  sensor_VL53L8CX_top.begin();
+
+  sensor_VL53L8CX_top.init_sensor();
+
+  sensor_VL53L8CX_top.vl53l8cx_set_resolution(res);
+
+  Serial.println("sensing initial dimensions");
+  measure_trashcan(trashcan_dimensions);
+
+  // Set interrupt pin
+  pinMode(INT_PIN, INPUT_PULLUP);
+  attachInterrupt(INT_PIN, measure, FALLING);
+
+  // Disable thresholds detection.
+  sensor_VL53L8CX_top.vl53l8cx_set_detection_thresholds_enable(0U);
+
+  setThresholds(100, trashcan_full);
+  
+
+  Serial.println("starting to measure");
+  // Start Measurements.
+  sensor_VL53L8CX_top.vl53l8cx_start_ranging();   
+}
+
+void loop()
+{
+
+  VL53L8CX_ResultsData Results;
+  uint8_t NewDataReady = 0;
+  uint8_t status;
+  
+  do {
+    status = sensor_VL53L8CX_top.vl53l8cx_check_data_ready(&NewDataReady);
+  } while (!NewDataReady);
+
+  if ((!status) && (NewDataReady != 0)&& interruptCount ) {
+    status = sensor_VL53L8CX_top.vl53l8cx_get_ranging_data(&Results);
+    // print_result(&Results);
+    trashcan_full = !trashcan_full;
+    Serial.println("trashcan " + String((trashcan_full)?"full":"empty"));
+    lora_in_progress = true;
+    notifyViaLora();
+    // execute scheduled jobs and events for Lora
+    while(lora_in_progress) {
+      os_runloop_once();
+      delay(1);
+    }
+    setThresholds((trashcan_full)?900:100, trashcan_full);
+    interruptCount = 0;
+    delay(1000);
+  }
+
+}
+
+void measure_trashcan(long trashcan_dimensions[]) {
+  sensor_VL53L8CX_top.vl53l8cx_start_ranging(); 
+  int initialized = 3;
+  VL53L8CX_ResultsData Results;
+  while(initialized>0) {
+    NewDataReady = 0;
+    do {
+      status = sensor_VL53L8CX_top.vl53l8cx_check_data_ready(&NewDataReady);
+    } while (!NewDataReady);
+
+    if ((!status) && (NewDataReady != 0)) {
+      status = sensor_VL53L8CX_top.vl53l8cx_get_ranging_data(&Results);
+      // long * dataStr = [];
+      //convert floats to string and assemble c-type char string for writing:
+      // dataStr += String(millis()-measurementStartTime) + ":";//add it onto the end
+
+      for (j = 0; j < number_of_zones; j ++)
+      {
+        //perform data processing here...
+        if((long)(&Results)->target_status[j] !=255){
+          trashcan_dimensions[j] = (long)(&Results)->distance_mm[j];
+        }
+      }
+      initialized--;
+    }
+  }
+  for (j = 0; j < number_of_zones; j += zones_per_line)
+  {  
+    for (l = 0; l < VL53L8CX_NB_TARGET_PER_ZONE; l++)
+    {
+      // Print distance and status.
+      for (k = (zones_per_line - 1); k >= 0; k--)
+      {
+        SerialPort.print(" ");
+        SerialPort.print(trashcan_dimensions[(VL53L8CX_NB_TARGET_PER_ZONE * (j+k)) + l]);
+        SerialPort.print(" ");
+      }
+    }
+    SerialPort.println("");
+  }
+  sensor_VL53L8CX_top.vl53l8cx_stop_ranging();
+}
+
+void tx_callback(osjob_t* job) {
+  lora_in_progress = false;
+}
+
+void notifyViaLora() {
+  tx(String(trashcan_full).c_str(), tx_callback); 
+}
+
+void print_result(VL53L8CX_ResultsData *Result)
+{
+  int8_t i, j, k, l;
+  uint8_t zones_per_line;
+  uint8_t number_of_zones = res;
+    
+  zones_per_line = (number_of_zones == 16) ? 4 : 8;
+
+  SerialPort.print("\n\n");
+  for (j = 0; j < number_of_zones; j += zones_per_line)
+  {  
+    for (l = 0; l < VL53L8CX_NB_TARGET_PER_ZONE; l++)
+    {
+      // Print distance and status.
+      for (k = (zones_per_line - 1); k >= 0; k--)
+      {
+        SerialPort.print(" ");
+        SerialPort.print((long)Result->distance_mm[(VL53L8CX_NB_TARGET_PER_ZONE * (j+k)) + l]);
+        SerialPort.print(" ");
+      }
+      SerialPort.println("");
+    }
+  }
+  SerialPort.print("\n");
+}
+
+void measure(void)
+{
+  interruptCount = 1;
+}
+
+void setThresholds(int threshold, bool far) {
+  // Set all values to 0.
+  memset(&thresholds, 0, sizeof(thresholds));
+  // Configure thresholds on each active zone
+  for (i = 0; i < res; i++)
+  {
+    if((!far && trashcan_dimensions[i]>threshold) || (far && trashcan_dimensions[i]<threshold)) {
+      thresholds[i].zone_num = i;
+      thresholds[i].measurement = VL53L8CX_DISTANCE_MM;
+      thresholds[i].type = (!far) ? VL53L8CX_LESS_THAN_EQUAL_MIN_CHECKER : VL53L8CX_GREATER_THAN_MAX_CHECKER;
+      thresholds[i].mathematic_operation = VL53L8CX_OPERATION_NONE;
+      thresholds[i].param_low_thresh = threshold;
+      thresholds[i].param_high_thresh = threshold;
+    }
+  }
+
+
+  // Last threshold must be clearly indicated.
+  thresholds[i].zone_num |= VL53L8CX_LAST_THRESHOLD;
+
+  // Send array of thresholds to the sensor.
+  sensor_VL53L8CX_top.vl53l8cx_set_detection_thresholds(thresholds);
+
+  // Enable thresholds detection.
+  sensor_VL53L8CX_top.vl53l8cx_set_detection_thresholds_enable(1U);
+}
